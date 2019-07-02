5c95cfe89a04dccfba41017168c23e6824ff9e6e
Hafiz Naeem <naeem,alpha2labs1@gmail.com>
2019-07-02 12:42:18 +0500

    message

--- app/src/main/java/com/example/muslima/Remote/ApiInterface.java
+++ app/src/main/java/com/example/muslima/Remote/ApiInterface.java
@@ -1,6 +1,13 @@
 package com.example.muslima.Remote;
 
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface ApiInterface {
+
+    @FormUrlEncoded
+    @POST()
+    String getData();
 
 
 }
